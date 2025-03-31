3fa972f10696a83a3c5627dcec33143db3fd2448
lucasvreis <lvreis@usp.br>
2025-03-31 08:23:16 -0300

    tidy

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "Alaoglu Borel Carrasco Cocycles Fubini Fubini's Lyapunov Measurability Pesin diffeomorphisms disintegrations preimage surjection")
+         (jinx-languages . "en_US"))))
