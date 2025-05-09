9d2b1e40dc7d91dbd44ae2a075702955586f2313
lucasvreis <lvreis@usp.br>
2025-05-09 15:03:14 -0300

    .

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((jinx-dir-local-words . "Alaoglu Borel Carrasco Cocycles Fubini Fubini's Lyapunov Measurability Pesin diffeomorphisms disintegrations preimage surjection")
-         (jinx-languages . "en_US"))))
+         (jinx-languages . "en_US")))
+ (LaTeX-mode . ((citar-bibliography . ("~/Zotero/bibs/all.bib"
+                                       "~/dados/arquivo/2024/mestrado/dissertation/mestrado.bib")))))
