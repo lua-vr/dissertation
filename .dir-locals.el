bb2e3f49812b4e043616d1494eec8e2590bf2e58
Lua Viana Reis <me@lua.blog.br>
2026-03-11 22:14:57 -0300

    save!!!!!!!!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "Alaoglu Borel Carrasco Cesàro Cocycles Donsker's Fubini Fubini's Kiriri Koopman Lipschitz Lyapunov Measurability Pesin Ruelle Schwarz Skorohod adjoint attractors coboundary diagonalization diffeomorphisms disintegrations equicontinuity equicontinuous foliation homeomorphisms homographies homography isometry metrizable nonuniformly observables parametrized preimage summable surjection")
+((nil . ((jinx-dir-local-words . "Alaoglu Borel Carrasco Cesàro Cocycles Donsker's Fubini Fubini's Kiriri Koopman Lipschitz Lyapunov Measurability Pesin Poincaré Ruelle Schwarz Skorohod adjoint attractors coboundary diagonalization diffeomorphisms disintegrations equicontinuity equicontinuous foliation homeomorphisms homographies homography isometry metrizable nonuniformly observables parametrized preimage summable surjection")
          (jinx-languages . "en_US")))
  (LaTeX-mode . ((abbrev/math-text-lang . en)
                 (citar-bibliography . ("~/Zotero/bibs/all.bib" "~/dados/arquivo/2024/mestrado/dissertation/mestrado.bib")))))
